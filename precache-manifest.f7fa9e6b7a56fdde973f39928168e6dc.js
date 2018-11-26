c886845f95e05bc2b341d9910ceb1135f673a195
MirEnovi <garciaivonne11@gmail.com>
2018-11-25 23:03:27 -0600

    Updates

---
+++ precache-manifest.f7fa9e6b7a56fdde973f39928168e6dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7b93e89e47ccd185cd1",
+    "url": "/yaydoo-technical-test/static/js/runtime~main.c7b93e89.js"
+  },
+  {
+    "revision": "c6f82a9769dbd4a2823a",
+    "url": "/yaydoo-technical-test/static/js/main.c6f82a97.chunk.js"
+  },
+  {
+    "revision": "bf2d07f8e371447e27a6",
+    "url": "/yaydoo-technical-test/static/js/1.bf2d07f8.chunk.js"
+  },
+  {
+    "revision": "c6f82a9769dbd4a2823a",
+    "url": "/yaydoo-technical-test/static/css/main.0b9166b6.chunk.css"
+  },
+  {
+    "revision": "6417809b373c3c5491dfd37793052416",
+    "url": "/yaydoo-technical-test/index.html"
+  }
+];
